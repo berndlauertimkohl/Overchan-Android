a5eed8cf730a637497ca99c04f61710a38e2669e
berndlauertimkohl <38379480+berndlauertimkohl@users.noreply.github.com>
2018-04-14 17:34:39 +0000

    Update KohlchanModule.java

--- src/nya/miku/wishmaster/chans/kohlchan/KohlchanModule.java
+++ src/nya/miku/wishmaster/chans/kohlchan/KohlchanModule.java
@@ -36,8 +36,8 @@ import nya.miku.wishmaster.api.util.ChanModels;
 public class KohlchanModule extends AbstractVichanModule {
     private static final String CHAN_NAME = "kohlchan.net";
     private static final SimpleBoardModel[] BOARDS = new SimpleBoardModel[] {
-            ChanModels.obtainSimpleBoardModel(CHAN_NAME, "b", "b", "VIP", false),
-            ChanModels.obtainSimpleBoardModel(CHAN_NAME, "int", "int", "VIP", false),
+            ChanModels.obtainSimpleBoardModel(CHAN_NAME, "b", "b", "VIP", true),
+            ChanModels.obtainSimpleBoardModel(CHAN_NAME, "int", "int", "VIP", true),
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "ernst", "ernst", "VIP", false),
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "helmut", "helmut", "VIP", false),
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "a", "Animu", "Allgemein", false),
@@ -55,7 +55,7 @@ public class KohlchanModule extends AbstractVichanModule {
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "ng", "NG", "Allgemein", false),
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "pol", "Politik&Philosophie", "Allgemein", false),
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "prog", "Programmieren", "Allgemein", false),
-            ChanModels.obtainSimpleBoardModel(CHAN_NAME, "s", "Schönes", "Allgemein", false),
+            ChanModels.obtainSimpleBoardModel(CHAN_NAME, "s", "Schönes", "Allgemein", true),
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "tv", "TV&VHS&Musik", "Allgemein", false),
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "v", "Videospiele", "Allgemein", false),
             ChanModels.obtainSimpleBoardModel(CHAN_NAME, "we", "Weltschmerz", "Allgemein", false),
